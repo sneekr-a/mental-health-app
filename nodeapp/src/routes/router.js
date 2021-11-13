1e6f4c6a1fca52594cfc822dd495693f03e62d24
zaccaffey <caffeyzac@gmail.com>
2021-11-13 14:21:23 -0600

    changes to router.js to add post

--- nodeapp/src/routes/router.js
+++ nodeapp/src/routes/router.js
@@ -1,7 +1,9 @@
 var user = require('./api/user')
+const post = require('./api/post')
 
 function setupRoute (app) {
     app.use('/user', user);
+    app.use('/post', post);
 }
 
 module.exports = setupRoute;
