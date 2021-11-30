91bae9f8ee8dc7e412c46c822805ef3ab79b5ab8
Zac Caffey <69201013+zaccaffey@users.noreply.github.com>
2021-11-29 21:05:18 -0600

    auth.js controller

--- nodeapp/src/routes/controller/auth.js
+++ nodeapp/src/routes/controller/auth.js
@@ -5,18 +5,20 @@ const {
    createJWT,
 } = require("../utils/auth");
 
-exports.signup = (req, res, next) => {                                                                                     // sign up auth
+exports.signup = (req, res, next) => {                                                                                 // sign up auth
   let { name, email, password, password_confirmation } = req.body;
+
   User.findOne({email: email})
    .then(user=>{
-      if(user){                                                                                                          // first check if the user exists
-         return res.status(422).json({ errors: [{ user: "email already linked with existing account" }] });             // if the user exists throw error
+      if(user){                                                                                                        // first check if the user exists
+         return res.status(422).json({ errors: [{ user: "email already linked with existing account" }] });            // if the user exists throw error
       }else {                                                                                                          // if the user doesn't then create a new user
          const user = new User({
            email: email,
            name: name,
            password: password,
          });
+
          bcrypt.genSalt(10, function(err, salt) { bcrypt.hash(password, salt, function(err, hash) { // hash the password before storing
          if (err) throw err;
          user.password = hash;
@@ -43,6 +45,7 @@ exports.signup = (req, res, next) => {                                          
 }
 exports.signin = (req, res) => {                                                                    // sign in auth
      let { email, password } = req.body;
+     
      User.findOne({ email: email }).then(user => {
        if (!user) {                                                                                 // check to see if the user exists
          return res.status(404).json({
@@ -54,15 +57,18 @@ exports.signin = (req, res) => {                                                
               return res.status(400).json({ errors: [{ password: "incorrect" }]                    // if password do not match then throw an error 
               });
              }
+
        let access_token = createJWT(                                                                
          user.email,
          user._id,
          3600
        );
+
        jwt.verify(access_token, process.env.TOKEN_SECRET, (err, decoded) => {                       // sign our JWT token
          if (err) {
             res.status(500).json({ erros: err });
          }
+         
          if (decoded) {
              return res.status(200).json({                                                          // if we successfully decode the access token send a success token
                 success: true,
