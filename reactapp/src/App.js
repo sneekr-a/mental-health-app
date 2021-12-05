63204483e5566fe114df492e3a7f7cff09d436d8
Zac Caffey <69201013+zaccaffey@users.noreply.github.com>
2021-12-05 12:09:53 -0600

    work on JWT auth

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Axios from 'axios';
 
 import RegisterUser from './components/RegisterUser';
 import LoginUser from './components/LoginUser';
-import HomePage from './components/HomePage'
+import HomePage from './components/HomePage';
+import Authenticate from './components/Authenticate';
 
 class App extends Component {
   render() {
@@ -22,4 +24,30 @@ class App extends Component {
   }
 }
 
+class Auth extends Component {
+  static contextType = AuthContext
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: {
+          email: '',
+          password: ''
+      },
+
+      error: {
+          message: '',
+          code: ''
+      },
+      isloading: false,
+      isLoginMode: true,
+
+      errors: {
+          email: '',
+          password: ''
+      }
+    }
+  }
+}
 export default App;
